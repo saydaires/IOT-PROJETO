3338c149ac644b4134e03250bbd8cc7f145f9245
Sayd Aires <159132545+saydaires@users.noreply.github.com>
2025-05-16 17:08:41 -0300

    Código Funcional!!!

--- script-dabble.ino
+++ script-dabble.ino
@@ -1,74 +1,92 @@
-/* 
-
-O que falta para o code abaixo compilar em outras máquinas:
-
-0. Em um dispositivo móvel ANDROID/IOS, instale o aplicativo "Arduino ESP Bluetooth - Dabble"
-
-1. em Tools > Board > Board Manager instale a board "esp32 by Espressif Systems" na VERSÃO 2.0.14;
-
-2. Após a instalação, em Tools > Board, selecione a board "ESP WROOM";
-
-3. No menu lateral esquerdo, selecione a opção "libraries" e baixe a biblioteca "DabbleESP32";
-
-4. Compile o código.
-
- */
+/* CONFIGS
+esp32 by Espressif Systems version 2.0.4
+board: esp32 WROOM
+*/
 
 #define CUSTOM_SETTINGS
 #define INCLUDE_GAMEPAD_MODULE
 #include <DabbleESP32.h>
 
 // Definição dos pinos conectados à ponte-H
-#define IN1 13
-#define IN2 12
-#define IN3 14
-#define IN4 27
+#define MOTOR_DIR_F 13 // Direita Frente
+#define MOTOR_DIR_R 12 // Direita Ré (Trás)
+#define MOTOR_ESQ_F 27 // Esquerda Frente
+#define MOTOR_ESQ_R 26 // Esquerda Ré (Trás)
 
 void setup() {
-  Serial.begin(115200);           // Inicializa a comunicação serial
-  Dabble.begin("SumoESP32");      // Inicializa o Dabble com o nome Bluetooth
+  Serial.begin(115200);              // Inicializa a comunicação serial
+  Dabble.begin("SumoESP32");         // Inicializa o Bluetooth com nome "SumoESP32"
 
   // Configura os pinos como saída
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
+  pinMode(MOTOR_DIR_F, OUTPUT);
+  pinMode(MOTOR_DIR_R, OUTPUT);
+  pinMode(MOTOR_ESQ_F, OUTPUT);
+  pinMode(MOTOR_ESQ_R, OUTPUT);
 
-  Stop(); // Garante que o robô inicie parado
+  stopMotors(); // Garante que o robô inicie parado
 }
 
 void loop() {
-  Dabble.processInput(); // Processa as entradas do Dabble
+  Dabble.processInput(); // Processa comandos do app Dabble
 
   if (GamePad.isUpPressed()) {
-    forward();
-  } else if (GamePad.isDownPressed()) {
-    backward();
-  } else {
-    Stop();
+    Serial.println("Comando: Frente");
+    moverFrente();
+  } 
+  else if (GamePad.isDownPressed()) {
+    Serial.println("Comando: Trás");
+    moverTras();
+  } 
+  else if (GamePad.isLeftPressed()) {
+    Serial.println("Comando: Esquerda");
+    virarEsquerda();
+  } 
+  else if (GamePad.isRightPressed()) {
+    Serial.println("Comando: Direita");
+    virarDireita();
+  } 
+  else {
+    Serial.println("Comando: Parar");
+    stopMotors();
   }
 }
 
 // Função para mover o robô para frente
-void forward() {
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
+void moverFrente() {
+  digitalWrite(MOTOR_DIR_F, HIGH);
+  digitalWrite(MOTOR_DIR_R, LOW);
+  digitalWrite(MOTOR_ESQ_F, HIGH);
+  digitalWrite(MOTOR_ESQ_R, LOW);
 }
 
 // Função para mover o robô para trás
-void backward() {
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
+void moverTras() {
+  digitalWrite(MOTOR_DIR_F, LOW);
+  digitalWrite(MOTOR_DIR_R, HIGH);
+  digitalWrite(MOTOR_ESQ_F, LOW);
+  digitalWrite(MOTOR_ESQ_R, HIGH);
+}
+
+// Função para virar para a esquerda (parar motor esquerdo, girar direito para frente)
+void virarEsquerda() {
+  digitalWrite(MOTOR_DIR_F, HIGH);
+  digitalWrite(MOTOR_DIR_R, LOW);
+  digitalWrite(MOTOR_ESQ_F, LOW);
+  digitalWrite(MOTOR_ESQ_R, LOW);
+}
+
+// Função para virar para a direita (parar motor direito, girar esquerdo para frente)
+void virarDireita() {
+  digitalWrite(MOTOR_DIR_F, LOW);
+  digitalWrite(MOTOR_DIR_R, LOW);
+  digitalWrite(MOTOR_ESQ_F, HIGH);
+  digitalWrite(MOTOR_ESQ_R, LOW);
 }
 
 // Função para parar o robô
-void Stop() {
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
+void stopMotors() {
+  digitalWrite(MOTOR_DIR_F, LOW);
+  digitalWrite(MOTOR_DIR_R, LOW);
+  digitalWrite(MOTOR_ESQ_F, LOW);
+  digitalWrite(MOTOR_ESQ_R, LOW);
 }
